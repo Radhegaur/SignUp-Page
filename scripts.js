331a8aa29e7ed268e634689f890ffdc0ce333484
Radhe <radheshyamprasad343@gmail.com>
2024-03-02 22:57:30 +0530

    Complete Signup Page

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const container = document.querySelector(".container");
+const SignUpBtn = document.querySelector(".green-bg button");
+
+SignUpBtn.addEventListener(
+    'click',
+    () =>{
+        container.classList.toggle('change');
+    }
+);
